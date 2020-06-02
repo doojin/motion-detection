edad3823d2b9f7946ed44fa7ccae0d4134a12239
doojin <dmitry.papka@gmail.com>
2020-06-02 17:03:46 -0500

    displaying video on canvas

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-   console.log('document loaded');
+function drawVideo(video, canvas, canvasContext) {
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+    canvasContext.drawImage(video, 0, 0, canvas.width, canvas.height);
+    requestAnimationFrame(() => drawVideo(video, canvas, canvasContext));
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const video = document.createElement('video');
+    video.srcObject = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+    await video.play();
+
+    const canvas = document.querySelector('canvas');
+    const context = canvas.getContext('2d');
+    drawVideo(video, canvas, context);
 });
